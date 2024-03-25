a32867ad26e967a83d52a3841011b8ab56659356
Atademes <156687124+Atademes@users.noreply.github.com>
2024-03-25 14:35:58 -0300

    C:\Users\Atademes\Documents\hello3.js

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hello World from Sao Paulo in Brazil!");
+    expect(hello.hello()).toEqual("Hello World from Brazil!");
   });
 });
