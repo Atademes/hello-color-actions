c5c218118ff3883361f1cd50ef15aedc38f0148c
Atademes <156687124+Atademes@users.noreply.github.com>
2025-04-07 03:29:01 -0300

    Add files via upload

--- hello.js
+++ hello.js
@@ -1,3 +1,4 @@
-const hello = () => "Hello, I'm from Sao Paulo";
+const hello = () => "Hello World from Brazil!";
 console.log(hello()); // Corrigido: sem argumentos e com parênteses corretos
 exports.hello = hello;
+
