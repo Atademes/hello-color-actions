154ed56422efb7305754b240f593ade5e5b43812
Atademes <156687124+Atademes@users.noreply.github.com>
2025-04-07 03:47:09 -0300

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,4 +1,3 @@
-const hello = () => "Hello World from Brazil!";
-console.log(hello()); // Corrigido: sem argumentos e com parênteses corretos
-exports.hello = hello;
-
+function hello() {
+  return "Hello World from Brazil!";
+}
