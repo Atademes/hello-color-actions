5ca3305e4a16fd6a79275581bd4ff8ebe37ed958
celso131313 <celsolimasilva@yahoo.com.br>
2025-04-06 23:00:16 -0300

    Corrige chamada da função hello para teste de CI/CD

--- hello.js
+++ hello.js
@@ -1,3 +1,4 @@
 const hello = () => "Hello World from the Office Hours in the terminal!";
-console.log(hello("Hello World! Teste de CI/CD funcionando 😎"));
+console.log(hello()); // Corrigido: sem argumentos e com parênteses corretos
 exports.hello = hello;
+
