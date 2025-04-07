d3a4cb53a3080257d597d52cff90d51344057eb3
Atademes <156687124+Atademes@users.noreply.github.com>
2025-04-07 17:15:13 -0300

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,5 +1,5 @@
 const hello = require('./hello');
 
 test('returns correct greeting', () => {
-  expect(hello()).toBe("Hello World from Sao Paulo");
+  expect(hello()).toBe("Hello, I'm from Sao Paulo");
 });
